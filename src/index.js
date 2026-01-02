5ad80bd54ec8e4b238a71a60337932ec50e1fa32
Yas5584 <ys136450@gmail.com>
2026-01-02 10:39:12 +0530

    database code

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// rquire('dotenv').config({path:'./.env'})
+import connectDB from "./db/index.js"
+import dotenv from 'dotenv'
+
+dotenv.config({
+    path: './env'
+})
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (async()=>{
+//     try {
+       
+//          await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+//     }
+//     catch (error) {
+//         console.log(error);
+//     }
+
+// })()
